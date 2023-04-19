d6cede2c1c3ff3b3fa293bdd90900b9d71bef298
Genderson Barinas Guio <genderson.barinas@gmail.com>
2023-04-18 22:13:34 -0500

    working with lists and loops

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+
+        int sum = numbers.Sum();
+        decimal average = (decimal)sum / (numbers.Count - 1);
+        int largestNumber = numbers.OrderDescending().FirstOrDefault();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
