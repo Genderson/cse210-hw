556a10abd5af21eb157cd105833af5553deb233a
Genderson Barinas Guio <genderson.barinas@gmail.com>
2023-04-21 20:28:10 -0500

    methods added to resolve the exercise

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+
+    public static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    public static string PromptUserName(){
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    public static int PromptUserNumber(){
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    public static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    public static void DisplayResult(string userName, int square)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {square}");
     }
 }
