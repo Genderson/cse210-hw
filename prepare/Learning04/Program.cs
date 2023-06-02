5e1c3680ac0519c4b8f2eb12af4e454b4bc12491
Genderson Barinas Guio <genderson.barinas@gmail.com>
2023-06-01 19:39:10 -0500

    homework completed

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using Learning04;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+    {   
+        Console.Clear();
+
+        Assignment assigmentMultiplication = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assigmentMultiplication.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment assigmentFraction = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assigmentFraction.GetSummary());
+        Console.WriteLine(assigmentFraction.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment assigmentHistory = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assigmentHistory.GetSummary());
+        Console.WriteLine(assigmentHistory.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learning04
+{
+    public class WritingAssignment : Assignment
+    {
+        private string _title;
+        public WritingAssignment(string studentName, string topic, string title): base(studentName, topic)
+        {
+            _title = title;
+        }
+
+        public string GetWritingInformation()
+        {
+            return $"{_title} by {GetStudentName()}";
+        }
+    }
+}
